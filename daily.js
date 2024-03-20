0e369ac545111fd37583d857ffd281bdd2248d86
Tanja <bondarenko.tanjuskin@gmail.com>
2024-03-20 12:11:55 +0200

    change name of main html file

--- daily.js
+++ daily.js
@@ -3,57 +3,78 @@ class DailyForecast{
     constructor() {
         
         this.dailyForecastBlock = document.querySelectorAll(".dailyForecastBlock")
-        this.dailyFeelLike = document.createElement("div")
-        this.maxTemparature = document.createElement("div")
-        this.minTemparature = document.createElement("div")
-        this.maxTemparature = document.createElement("div")
-        this.dailyVind = document.createElement("div")
-        this.dailyPressure = document.createElement("div")
-        this.dailyHumidity = document.createElement("div")
         this.block
         this.dayBlock
+        this.dateNameList = new Object();
+        this.namesOfDay = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Sutarday"]
     }
     
     createDailyForecast(link) {
         this.findCallingBlockName(link)
-        let newDate;
-        let dateArr = []
+        let dateList = []
         let date = new Date();
         fetch(link)
             .then(response => response.json())
             .then(json => {
                 let list = json.list
                 list.forEach(el => {
+                   
                     let elDate = new Date(+(`${el.dt}` + "000")).toLocaleDateString()
-                    if (date.toLocaleDateString() === elDate) {
+                    let nameOfDay = this.namesOfDay[new Date(+(`${el.dt}` + "000")).getDay()]
+                      if (date.toLocaleDateString() === elDate) {
                         return
                     } else {
-                        dateArr.push(elDate)
+                          if(dateList.includes(elDate)){
+                            return
+                          }else{dateList.push(elDate)
+                            this.dateNameList[`${elDate}`] = nameOfDay;
+                        }
                     }
                 })
-                let newDate = new Set(dateArr)
+
+                let newDate = new Set(dateList)
                 this.createDayBlock(newDate)
                 let days = document.querySelectorAll(".day_block")
                     list.forEach(element => {
                         for (let day of days) {
                             if(day.parentElement.dataset.city == this.block){
-                            console.log(day)
                         if (day.dataset.date == (new Date(+(`${element.dt}` + "000")).toLocaleDateString()) && (new Date(+(`${element.dt}` + "000")).toLocaleTimeString()) == "02:00:00") {
-                        this.createCurrentDay()
-                        this.dailySky.src = "http://openweathermap.org/img/wn/" + element.weather[0]['icon'] + "@2x.png"
-                        this.dailyDescription.innerHTML = element.weather[0].description
-                        this.dailyTemp.innerHTML = Math.round(element.main.temp - 273.15) + "C"
-                        day.append(this.dailySky)
-                        day.append(this.dailyDescription)
-                        day.append(this.dailyTemp)
-                        }}
+                        this.createCurrentDay();
+                        this.dailySkyBlock = document.createElement("div");
+                        this.dailySkyBlock.classList.add("dailySkyBlock");
 
-                    }
-                    
-                    
+                        this.dailySky.src = "http://openweathermap.org/img/wn/" + element.weather[0]['icon'] + "@2x.png";
+                        this.dailyDescription.innerHTML = element.weather[0].description;
+                        
+                        this.dailyTempBlock = document.createElement("div");
+                        this.dailyTempBlock.classList.add("dailyTempBlock");
+                        this.dailyTemp.innerHTML = Math.round(element.main.temp - 273.15) + "C";
+                        this.dailyFeelLike.innerHTML = "Feels Like " + Math.round(element.main.feels_like - 273.15) + "C";
+
+                        this.dailyRestBlock = document.createElement("div");
+                        this.dailyRestBlock.classList.add("dailyRestBlock");
+
+                        this.dailyHumidity.innerHTML = "Humidity " + element.main.humidity + "%";
+                        this.dailyPressure.innerHTML = "Atmospheric pressure " + element.main.pressure + "hPA";
+                        this.dailyVind.innerHTML = "Wind speed: " + element.wind.speed + "metre/sec";
+                        
+                        day.append(this.dailySkyBlock);
+                        this.dailySkyBlock.append(this.dailySky);
+                        this.dailySkyBlock.append(this.dailyDescription);
+
+                        day.append(this.dailyTempBlock);
+                        this.dailyTempBlock.append(this.dailyTemp);
+                        this.dailyTempBlock.append(this.dailyFeelLike);
+
+                        day.append(this.dailyRestBlock);
+                        this.dailyRestBlock.append(this.dailyHumidity);
+                        this.dailyRestBlock.append(this.dailyPressure);
+                        this.dailyRestBlock.append(this.dailyVind);
+                        }}
+                    }  
             }) 
         })        
-                }
+     }
     findCallingBlockName(data) {
         for (let [key, value] of Object.entries(dailyLink)) {
             if (data == `${value}`) {
@@ -70,32 +91,41 @@ class DailyForecast{
                     data.forEach(el => {
                         this.dayBlock = document.createElement("div")
                         this.dayBlock.classList.add("day_block")
-                        this.dayDiv =document.createElement("div")
+                        this.dayDiv = document.createElement("div")
+                        this.nameOfDayP = document.createElement("p")
+                        this.nameOfDayP.innerHTML = this.dateNameList[el]
+                        
                         this.dayDiv.classList.add("dayDiv")
-
                         this.dayBlock.dataset.date = el
                         this.dayDiv.innerHTML = el
                         this.dayBlock.append(this.dayDiv)
                         element.append(this.dayBlock)
-                       
-
-
+                        this.dayDiv.append(this.nameOfDayP)
+            
                     })
                 }
             }
         }
-        
-       
-        
-        
     }
      createCurrentDay(){
-                        this.dailySky = document.createElement("img")
-                        this.dailyDescription = document.createElement("div")
-                        this.dailyTemp = document.createElement("div")
-                        this.dailySky.classList.add("dailySky")
-                        this.dailyDescription.classList.add("dailyDescription")
-                        this.dailyTemp.classList.add("dailyTemp") 
-                    
+    this.dailySky = document.createElement("img");
+    this.dailyDescription = document.createElement("div");
+    this.dailyTemp = document.createElement("div");
+    this.dailyTemp.classList.add("dailyTemp"); 
+
+    this.dailySkyDesciptBlock = document.createElement("div");
+    this.dailySkyDesciptBlock.classList.add = "dailySkyDesciptBlock";
+
+    this.dailySky.classList.add("dailySky");
+    this.dailyDescription.classList.add("dailyDescription");
+    
+    this.dailyFeelLike = document.createElement("div");
+    this.dailyFeelLike.classList.add("dailyFeelLike")
+
+    this.dailyRestBlock = document.createElement("div");
+    this.dailyRestBlock.classList.add("dailyRestBlock"); 
+    this.dailyVind = document.createElement("div");
+    this.dailyPressure = document.createElement("div");
+    this.dailyHumidity = document.createElement("div");           
      }
 }
--- main1.js
+++ main1.js
@@ -4,7 +4,6 @@ let link = [
     londonLink = "http://api.openweathermap.org/data/2.5/weather?id=2643743&appid=bf35cac91880cb98375230fb443a116f&units=metric",
     newYorkLink = "http://api.openweathermap.org/data/2.5/weather?id=5128638&appid=bf35cac91880cb98375230fb443a116f&units=metric",
 ]
-
 let dailyLink = {
     Kyiv: "http://api.openweathermap.org/data/2.5/forecast?q=Kyiv&appid=ee6e6f1c8c4fb651379b1c6860d3cb27",
     London: "http://api.openweathermap.org/data/2.5/forecast?q=London&appid=b644f05b948b65eda83251ab3f18331f",
@@ -14,28 +13,27 @@ document.querySelector("#currentDate").innerHTML = new Date().toLocaleDateString
 
 class Forecast {
     constructor() {
-        this.main = document.querySelector("main")
-        this.city = document.createElement("div")
-        this.blockForecust = document.createElement("div")
-        this.dailyForecast = document.createElement("button")
-        this.dailyForecastBlock = document.createElement("div")
-        this.dailyForecastBlock.classList.add("dailyForecastBlock")
-        this.degreesBlock = document.createElement("div")
-        this.degree = document.createElement("div")
-        this.feelsLike = document.createElement("div")
-        this.btnCelsius = document.createElement("button")
-        this.skyBlock = document.createElement("div")
-        this.sky = document.createElement("img")
-        this.skyDeskription = document.createElement("div")
-        this.btnDetail = document.createElement("button")
-        this.blockDetail = document.createElement("div")
-        this.sunRise = document.createElement("div")
-        this.sunSet = document.createElement("div")
-        this.pressure = document.createElement("div")
-        this.humidity = document.createElement("div")
-        this.visibility = document.createElement("div")
-        this.gust = document.createElement("div")
-        this.speed = document.createElement("div")
+        this.main = document.querySelector("main");
+        this.city = document.createElement("div");
+        this.blockForecust = document.createElement("div");
+        this.dailyForecast = document.createElement("button");
+        this.dailyForecastBlock = document.createElement("div");
+        this.dailyForecastBlock.classList.add("dailyForecastBlock");
+        this.degreesBlock = document.createElement("div");
+        this.degree = document.createElement("div");
+        this.feelsLike = document.createElement("div");
+        this.btnCelsius = document.createElement("button");
+        this.skyBlock = document.createElement("div");
+        this.sky = document.createElement("img");
+        this.skyDeskription = document.createElement("div");
+        this.btnDetail = document.createElement("button");
+        this.blockDetail = document.createElement("div");
+        this.sunRise = document.createElement("div");
+        this.sunSet = document.createElement("div");
+        this.pressure = document.createElement("div");
+        this.humidity = document.createElement("div");
+        this.visibility = document.createElement("div");
+        this.speed = document.createElement("div");
     }
     createForecast(link) {
         fetch(link)
@@ -46,102 +44,98 @@ class Forecast {
                 let arr = []
                 json.name.split("").forEach(el =>
                 { if (el === " ") el = "_"; arr.push(el) })
-
-                this.dailyForecast.dataset.city = this.dailyForecastBlock.dataset.city = arr.join("")
-
-                
-                this.dailyForecast.addEventListener("click", (e) => {         
+                this.dailyForecast.dataset.city = this.dailyForecastBlock.dataset.city = arr.join("");             
+                this.dailyForecast.addEventListener("click", (e) => { 
+                    if(this.blockDetail.style.display == "flex"){this.blockDetail.style.display = "none"}        
                     if (this.dailyForecastBlock.style.display == "none" && this.dailyForecastBlock.children.length == 0) {
-                        this.dailyForecastBlock.style.display = "flex"
-                        let dailyForecasts = new DailyForecast()
-                        let nameLink = e.target.dataset.city
+                        this.dailyForecastBlock.style.display = "flex";
+                        let dailyForecasts = new DailyForecast();
+                        let nameLink = e.target.dataset.city;
                         for (let [key, value] of Object.entries(dailyLink)) {
                             if (`${key}` == nameLink) {
                                 dailyForecasts.createDailyForecast(`${value}`)
                             }
                         }
                     }else if (this.dailyForecastBlock.children && this.dailyForecastBlock.style.display == "flex") {
-                        this.dailyForecastBlock.style.display = "none"
-                        
+                        this.dailyForecastBlock.style.display = "none";
                     }else if (this.dailyForecastBlock.children && this.dailyForecastBlock.style.display == "none"){
-                        this.dailyForecastBlock.style.display = "flex"
-                        
+                        this.dailyForecastBlock.style.display = "flex";
                     }
-                  
-                    
-
-                   
                 })
-                this.createDegreesBlock()
+
+                this.createDegreesBlock();
                 this.degree.innerHTML = Math.round(json.main.temp) + "C";
                 this.feelsLike.innerHTML = "fels like " + Math.round(json.main.feels_like) + "C";
-                this.btnCelsius.addEventListener("click", () => this.showInCelsius(json))
-              this.createSkyBlock()
+                this.btnCelsius.addEventListener("click", () => this.showInCelsius(json));
+                this.createSkyBlock();
                 this.sky.src = "http://openweathermap.org/img/wn/" + json.weather[0]['icon'] + "@2x.png";
                 this.skyDeskription.innerHTML = json.weather[0].description;
                 this.sunRise.innerHTML = "Sunrise: " + new Date(+(`${json.sys.sunrise}` + "000")).toLocaleTimeString();
-                this.sunSet.innerHTML = "Sunset: " + new Date(+(`${json.sys.sunset}` + "000")).toLocaleTimeString()
+                this.sunSet.innerHTML = "Sunset: " + new Date(+(`${json.sys.sunset}` + "000")).toLocaleTimeString();
                 this.humidity.innerHTML = "Humidity: " + json.main.humidity + "%";
                 this.pressure.innerHTML = "Atmospheric pressure: " + json.main.pressure + " hPa";
                 this.visibility.innerHTML = "Visibility: " + json.visibility + " meter";
-                this.gust.innerHTML = "Wind gust: " + json.wind.gust + " meter/sec";
-                this.speed.innerHTML = " Wind speed: " + json.wind.speed + " meter/sec";
-                this.createDetailBlock()
+                this.speed.innerHTML = " Wind speed: " + json.wind.speed + " metre/sec";
+                this.createDetailBlock();
                 this.btnDetail.addEventListener("click", () => this.showDetailForecast())
             })
     }; 
+
     createCityBlock() {
-        this.city.classList.add("name")
-        this.blockForecust.classList.add("city")
-        this.divNameCity = document.createElement("div")
-        this.divNameCity.classList.add("city_name_block")
-        this.dailyForecast.classList.add("dailyForecast")
-        this.dailyForecast.innerHTML = "forecast for 5 days"
-        this.divNameCity.append(this.city)
-        this.divNameCity.append(this.dailyForecast)
-        this.blockForecust.append(this.divNameCity)
-       
+        this.city.classList.add("name");
+        this.blockForecust.classList.add("city");
+        this.divNameCity = document.createElement("div");
+        this.divNameCity.classList.add("city_name_block");
+        this.dailyForecast.classList.add("dailyForecast");
+        this.dailyForecast.innerHTML = "forecast for 5 days";
+        this.divNameCity.append(this.city);
+        this.divNameCity.append(this.dailyForecast);
+        this.blockForecust.append(this.divNameCity);
     };
+
     createDegreesBlock() {
-        this.degreesBlock.classList.add("degrees_block")
-        this.degree.classList.add("degrees")
-        this.feelsLike.classList.add("feels_like")
-        this.btnCelsius.classList.add("btn_in_celsius")
-        this.btnCelsius.innerHTML = "in Fahrenheit"
-        this.degreesBlock.append(this.degree)
-        this.degreesBlock.append(this.feelsLike)
-        this.degreesBlock.append(this.btnCelsius)
-        this.blockForecust.append(this.degreesBlock)
+        this.degreesBlock.classList.add("degrees_block");
+        this.degree.classList.add("degrees");
+        this.feelsLike.classList.add("feels_like");
+        this.btnCelsius.classList.add("btn_in_celsius");
+        this.btnCelsius.innerHTML = "in Fahrenheit";
+        this.degreesBlock.append(this.degree);
+        this.degreesBlock.append(this.feelsLike);
+        this.degreesBlock.append(this.btnCelsius);
+        this.blockForecust.append(this.degreesBlock);
     };
+
     createSkyBlock() {
-        this.skyBlock.classList.add("sky_block")
-        this.sky.classList.add("sky")
-        this.skyDeskription.classList.add("sky_description")
-        this.btnDetail.classList.add("btn_in_detail")
-        this.btnDetail.textContent = "detailed forecast"
-        this.skyBlock.append(this.sky)
-        this.skyBlock.append(this.skyDeskription)
-        this.skyBlock.append(this.btnDetail)
-        this.blockForecust.append(this.skyBlock)
+        this.skyBlock.classList.add("sky_block");
+        this.sky.classList.add("sky");
+        this.skyDeskription.classList.add("sky_description");
+        this.btnDetail.classList.add("btn_in_detail");
+        this.btnDetail.textContent = "detailed forecast";
+        this.skyBlock.append(this.sky);
+        this.skyBlock.append(this.skyDeskription);
+        this.skyBlock.append(this.btnDetail);
+        this.blockForecust.append(this.skyBlock);
     };
+
     createDetailBlock() {
-        this.blockDetail.classList.add("block_detail")
-        this.dailyForecastBlock.style.display = "none"
-        this.blockDetail.style.display = "none"
-        this.blockDetail.append(this.sunRise)
-        this.blockDetail.append(this.sunSet)
+        this.blockDetail.classList.add("block_detail");
+        this.dailyForecastBlock.style.display = "none";
+        this.blockDetail.style.display = "none";
+        this.blockDetail.append(this.sunRise);
+        this.blockDetail.append(this.sunSet);
         this.blockDetail.append(this.humidity);
         this.blockDetail.append(this.pressure);
         this.blockDetail.append(this.visibility);
         this.blockDetail.append(this.speed);
-        this.main.append(this.blockForecust)
-        this.main.append(this.blockDetail)
+        this.main.append(this.blockForecust);
+        this.main.append(this.blockDetail);
         this.blockForecust.after(this.dailyForecastBlock)
     };
+
     showInCelsius(data) {
         if (this.btnCelsius.innerHTML == "in Fahrenheit") {
             this.degree.innerHTML = Math.round(data.main.temp + 273.15) + "F";
-            this.btnCelsius.innerHTML = "in Celsius"
+            this.btnCelsius.innerHTML = "in Celsius";
             this.feelsLike.innerHTML = "feels like " + Math.round(data.main.feels_like + 273.15) + "F";
         } else {
             this.btnCelsius.innerHTML = "in Fahrenheit";
@@ -149,14 +143,14 @@ class Forecast {
             this.feelsLike.innerHTML = "feels like " + Math.round(data.main.feels_like) + " C";
         }
     };
+
     showDetailForecast() {
         if (this.blockDetail.style.display == "none") {
-            this.blockDetail.style.display = "flex"
+            if(this.dailyForecastBlock.style.display == "flex"){this.dailyForecastBlock.style.display = "none"};
+            this.blockDetail.style.display = "flex";
         } else { this.blockDetail.style.display = "none" }
     }
-    
 }
-
 
 let kievWeatherForecast = new Forecast();
 kievWeatherForecast.createForecast(kievLink);
